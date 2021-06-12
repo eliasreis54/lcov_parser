2c7a7c865d1653ab4c4c7a66b60ed4ef808909ff
Elias Reis <eliasreis54@gmail.com>
2021-06-11 23:11:06 -0300

    fix tests

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,5 +1,5 @@
 import 'package:args/args.dart';
-import 'package:lcov_parser/src/parser.dart';
+import 'package:lcov_parser/lcov_parser.dart';
 
 void main(List<String> args) async {
   final parser = ArgParser();
@@ -9,16 +9,21 @@ void main(List<String> args) async {
   final arguments = parser.parse(args);
 
   final file = arguments['file'] ?? '';
+  try {
+    final records = await Parser.parse(file);
 
-  final records = await Parser.parse(file);
+    var totalHits = 0;
+    var totalFinds = 0;
+    records.forEach((rec) {
+      totalFinds += rec.lines?.found ?? 0;
+      totalHits += rec.lines?.hit ?? 0;
+    });
 
-  var totalHits = 0;
-  var totalFinds = 0;
-  records.forEach((rec) {
-    totalFinds += rec.lines?.found ?? 0;
-    totalHits += rec.lines?.hit ?? 0;
-  });
-
-  final coverage = (totalHits / totalFinds) * 100;
-  print('Total coverage is : $coverage');
+    final coverage = (totalHits / totalFinds) * 100;
+    print('Total coverage is : $coverage');
+  } on FileMustBeProvided {
+    print('You need provide a file to parse');
+  } catch (e) {
+    print(e);
+  }
 }
